0832aefa8848b8184c82596de87a9e218bcec0d8
Bal <e1855035@cmaisonneuve.qc.ca>
2021-11-11 11:44:50 -0500

    ajout images profs + ajout gitignore

--- equipe3/template-parts/content-professeurs.php
+++ equipe3/template-parts/content-professeurs.php
@@ -9,38 +9,23 @@
 
 ?>
 // professeurs
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-			//the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		
-		 ?>
-	</header><!-- .entry-header -->
 
-	<?php equipe3_post_thumbnail(); ?>
+        <div class="site-info">
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'equipe3' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+            <div class="Professeurs">
+            <img class="Denis" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Denis.png" ?>"></img>
+            <img class="Greg" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Greg.png" ?>"></img>
+            <img class="Camille" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Camille.png" ?>"></img>
+            <img class="Vincent" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Vincent.png" ?>"></img>
+            <img class="Dominic" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Dominic.png" ?>"></img>
+            <img class="Eddy" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Eddy.png" ?>"></img>
+            <img class="Martin" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Martin.png" ?>"></img>
+            <img class="Vhaik" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Vhaik.png" ?>"></img>
+            <img class="Manon" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Manon.png" ?>"></img>
+            <img class="Ahmed" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Ahmed.png" ?>"></img>
+            <img class="Mathieu" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Mathieu.png" ?>"></img>
+            <img class="Caroline" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Caroline.png" ?>"></img>
+            <img class="David" src="<?php echo get_site_url() . "/wp-content/uploads/2021/11/Vincent.png" ?>"></img>
+            
 
-	
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php equipe3_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+        </div>
