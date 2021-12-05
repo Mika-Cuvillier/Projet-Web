0326ee4faefc556b50b845cd3ef11ddb42ef2ec9
SaraEmiliaBal <sara_emilia01@outlook.com>
2021-12-05 11:32:58 -0500

    modification du bouton X

--- equipe3/template-parts/content-professeurs.php
+++ equipe3/template-parts/content-professeurs.php
@@ -15,7 +15,7 @@
                     <?php echo the_post_thumbnail(); ?>
                     <div class="Informations">
                         <div class="bouton-fermeture">
-                            <button type="button">X</button>
+                            <button id="btnProf" type="button">X</button>
                         </div>
                         <h2 id="titreInfos"><?php echo get_the_content() ?></h2>
                     </div>
