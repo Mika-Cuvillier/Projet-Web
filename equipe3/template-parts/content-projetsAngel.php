65659469ea30ed6d46c4f14f3c54af6f8694eaa9
Montufar-Morejon <e1466061@cmaisonneuve.qc.ca>
2021-12-06 09:33:37 -0500

    debut projet restructure

---
+++ equipe3/template-parts/content-projetsAngel.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+<article class="projets">
+<a class="<?php echo $tPropriété['typeProjets']; ?>" href="<?php echo get_permalink() ?>">  <?php echo $tPropriété['titre']; ?><?php the_title(); ?></a>
+  <div class="content">
+<!--   <?php the_post_thumbnail('category-thumb'); ?> -->
+ <!--      <div class="content-overlay"><h3 class="content-title"><a href="<?php echo get_permalink(); ?>"> <?php the_title(); ?>    
+    
+    <div class="content-imageHover">-->
+    <!--   <div class="content-details fadeIn-bottom"> -->
+      
+  <!--   </div>
+  </div> -->
+</div> 
+</article>
