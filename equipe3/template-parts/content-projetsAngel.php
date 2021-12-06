2c3841d05ec7ad78ba422b5bdeb4fbd7f83e9a22
Montufar-Morejon <e1466061@cmaisonneuve.qc.ca>
2021-12-06 11:05:01 -0500

    projet restructure partie 2

--- equipe3/template-parts/content-projetsAngel.php
+++ equipe3/template-parts/content-projetsAngel.php
@@ -1,18 +1,30 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package equipe3
+ */
+global $tPropriété;
+
+?>
 
 
 
 
 
 <article class="projets">
+<div class="content">
 <a class="<?php echo $tPropriété['typeProjets']; ?>" href="<?php echo get_permalink() ?>">  <?php echo $tPropriété['titre']; ?><?php the_title(); ?></a>
-  <div class="content">
-<!--   <?php the_post_thumbnail('category-thumb'); ?> -->
- <!--      <div class="content-overlay"><h3 class="content-title"><a href="<?php echo get_permalink(); ?>"> <?php the_title(); ?>    
-    
-    <div class="content-imageHover">-->
-    <!--   <div class="content-details fadeIn-bottom"> -->
+<?php the_post_thumbnail('category-thumb'); ?>
+<!--  -->
+   <div class="content-overlay"><h3 class="content-title"><a href="<?php echo get_permalink(); ?>"> <?php the_title(); ?> 
+   </a></h3> <p> <?php echo get_the_content();?> </p></div>
+   <div class="content-imageHover">
+    <div class="content-details fadeIn-bottom"> 
       
-  <!--   </div>
-  </div> -->
-</div> 
+  </div>
+  </div> 
+
 </article>
